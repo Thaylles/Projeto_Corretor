3d8fbe8456fab19f5adcce23013f8f705ca84642
RodrigoGandra <rodrigo.gandra@hotmail.com>
2015-11-04 17:08:53 -0200

    main do pacote dicionário excluido

--- src/web/Main.java
+++ src/web/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		Dicionario dicionario = new Dicionario();
-		System.out.println(dicionario.isCorrect("banana"));
 		Spark.staticFileLocation("/publico");
 		
 		MustacheTemplateEngine engine = new MustacheTemplateEngine("apresentacao");
