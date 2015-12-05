eb8923cfc659505b46a4f98d3c93d6e7c5d9cc25
Thaylles <thayllesleal@hotmail.com>
2015-12-05 02:10:29 -0200

    htmls em andamento

--- src/distance/Main.java
+++ src/distance/Main.java
@@ -3,7 +3,7 @@ package distance;
 public class Main {
 
 	public static void main(String[] args) {
-		String a = "asar";
+		String a = "microondas";
 		System.out.println(Levenshtein.distance(a));
 
 	}
